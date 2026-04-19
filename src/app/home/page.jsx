d7316cc848edfa01eff2d381c3b3d6c1de60e129
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-04-19 21:38:50 +0600

    first commit

---
+++ src/app/home/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>hero page</h1>
+            <Button>hero button</Button>
+        </div>
+    );
+};
+
+export default page;
