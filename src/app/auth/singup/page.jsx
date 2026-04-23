db61c22b7a693af081fcfbb0ae71c12158843252
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-04-23 19:12:19 +0600

    explore singup with better auth

---
+++ src/app/auth/singup/page.jsx
@@ -0,0 +1,85 @@
+"use client";
+import React from 'react';
+import { Check } from "@gravity-ui/icons";
+import { Button, Description, FieldError, Form, Input, Label, TextField } from "@heroui/react";
+import { authClient } from '@/lib/auth.client';
+
+const singUpPage = () => {
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formdata = new FormData(e.currentTarget);
+        const userData = Object.fromEntries(formdata.entries());
+        // console.log(userData);
+        const { data, error } = await authClient.signUp.email({
+            name: userData.name,
+            email: userData.email,
+            password: userData.password,
+            // callbackURL: "https://example.com/callback",
+        });
+        console.log({ data, error });
+    }
+    return (
+        <div className='w-full h-screen flex items-center justify-center'>
+            <Form className="flex w-96 flex-col gap-4 border p-5 rounded-2xl shadow-2xl" onSubmit={onSubmit}>
+                <TextField
+                    isRequired
+                    name="name"
+                    type="text"
+                >
+                    <Label>Name</Label>
+                    <Input placeholder="Your name " />
+                    <FieldError />
+                </TextField>
+                <TextField
+                    isRequired
+                    name="email"
+                    type="email"
+                    validate={(value) => {
+                        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                            return "Please enter a valid email address";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Email</Label>
+                    <Input placeholder="john@example.com" />
+                    <FieldError />
+                </TextField>
+                <TextField
+                    isRequired
+                    minLength={8}
+                    name="password"
+                    type="password"
+                    validate={(value) => {
+                        if (value.length < 8) {
+                            return "Password must be at least 8 characters";
+                        }
+                        if (!/[A-Z]/.test(value)) {
+                            return "Password must contain at least one uppercase letter";
+                        }
+                        if (!/[0-9]/.test(value)) {
+                            return "Password must contain at least one number";
+                        }
+                        return null;
+                    }}
+                >
+                    <Label>Password</Label>
+                    <Input placeholder="Enter your password" />
+                    <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+                    <FieldError />
+                </TextField>
+                <div className="flex gap-2">
+                    <Button type="submit">
+                        <Check />
+                        Submit
+                    </Button>
+                    <Button type="reset" variant="secondary">
+                        Reset
+                    </Button>
+                </div>
+            </Form>
+        </div>
+    );
+};
+
+export default singUpPage;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
                 <ul className="flex items-center gap-4">
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/tasks">Tasks</Link></li>
+                    <li><Link href="/auth/singup">SingUp</Link></li>
                 </ul>
                 <div>
                     <ThemeSwitch></ThemeSwitch>
